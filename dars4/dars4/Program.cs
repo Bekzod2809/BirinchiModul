477ee4742012f793c36f359724a53e9efbb5c301
Bekzod <bekzodjonxasanov7@gmail.com>
2025-12-09 13:49:01 +0500

    4dars vazifa qo'shildi

--- dars4/dars4/Program.cs
+++ dars4/dars4/Program.cs
@@ -1,10 +1,83 @@
-﻿namespace dars4
+﻿namespace dars4;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        //string helloName = "Aziz";
+        //Console.WriteLine($" Hello {helloName}!");
+
+        //string a = "Hi";
+        //string b = "Bye";
+        //Console.WriteLine(a+b+b+a);
+
+        //string str = "Hello";
+        //Console.WriteLine(str.Substring(0,2));
+
+        //string satr = "Hi";
+        //string satr1 = (satr.Substring(satr.Length - 2));
+        //Console.WriteLine(satr1+satr1+satr1);
+
+        //string str = "WooHoo";
+        //int index = str.Length / 2;
+        //Console.WriteLine(str.Substring(0, index));
+
+        //string a = "Hello";
+        //string b = "Hi";
+        //int length1 = a.Length;
+        //int length2 = b.Length;
+        //string short1 = " ";
+        //string long1 = " ";
+
+        //if (length1 > length2)
+        //{
+        //    long1 = a;
+        //}
+        //else
+        //{
+        //    long1 = b;
+        //}
+        //if (length1 < length2)
+        //{
+        //    short1 = a;
+        //}
+        //else
+        //{
+        //    short1 = b;
+        //}
+        //Console.WriteLine(short1 + long1 + short1);
+
+        //string str = "Hello";
+        //string natija1 = str.Remove(0, 1);
+        //string str2 = "There";
+        //string natija2 = str2.Remove(0, 1);
+        //Console.WriteLine(natija1+natija2);
+
+        //string str = "Hello";
+        //string natija = str.Remove(0, 2);
+        //string natija1 = str.Remove(2, str.Length - 2);
+        //Console.WriteLine(natija + natija1);
+
+        //var num1 = 2;
+        //var num2 = 3;
+        //Console.WriteLine(num1+num2);
+
+        //string str = "xafli";
+        //Console.WriteLine(str.Replace('x','y'));
+
+        //int a = 10;
+        //int b = 20;
+        //int yigindi = 0;
+        //for (int i = a; i <= b; i++)
+        //{
+        //    if (i % 2 != 0)
+        //    {
+        //        yigindi += 1;
+        //    }
+        //}
+        //Console.WriteLine(yigindi);
+
+        int num = 123;
+        Console.WriteLine(num.ToString().Length);
     }
 }
