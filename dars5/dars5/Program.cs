c4debceb3359e86343020c93cc220c98ef7637d4
Bekzod <bekzodjonxasanov7@gmail.com>
2025-12-13 14:25:58 +0500

    5-dars qo'shildi

---
+++ dars5/dars5/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace dars5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //static int GetCountOfCapitalLetter(string s)
+            //{
+            //    int count = 0;
+            //    for (var i = 0; i < s.Length; i++)
+            //    {
+            //        if (char.IsUpper(s[i]))
+            //        {
+            //            count++;
+            //        }
+            //    }
+
+            //    return count;
+            //}
+            //var str = "Salom Dunyo! Hello World! G13 .net";
+            //Console.WriteLine(GetCountOfCapitalLetter(str));
+
+
+            //static string GetText(string name)
+            //{
+            //    if (name.Length > 5)
+            //    {
+            //        return name;
+            //    }
+            //    else
+            //    {
+            //        name = "Hello " + name;
+            //        return name;
+            //    }
+            //}
+            //Console.WriteLine(GetText("Aziz"));
+            //Console.WriteLine(GetText("Sarvar"));
+            //Console.WriteLine(GetText("Ali"));
+
+            //Console.Write("List sonini kiritng : ");
+            //var n = int.Parse(Console.ReadLine());
+            //List<int> ints = new List<int>();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("List elementlarini kiriting : ");
+            //    var nums = int.Parse(Console.ReadLine());
+            //    ints.Add(nums);
+            //}
+
+
+            //Console.Write("List sonini kiritng : ");
+            //var n = int.Parse(Console.ReadLine());
+            //List<int> ints = new List<int>();
+            //var counter = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("List elementlarini kiriting : ");
+            //    var nums = int.Parse(Console.ReadLine());
+            //    ints.Add(nums);
+            //    if (nums > 100)
+            //    {
+            //        counter = counter + 1;
+            //    }
+            //}
+            //Console.WriteLine($"{counter} ta 3 xonali son bor!");
+
+            //Console.Write("List sonini kiriting :");
+            //var n = int.Parse(Console.ReadLine());
+            //List<int> ints = new List<int>();
+            //var counter = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("List elementlarini kiriting :");
+            //    var nums = int.Parse(Console.ReadLine());
+            //    ints.Add(nums);
+            //    if (nums % 2 == 0)
+            //    {
+            //        counter = counter + 1;
+            //    }
+            //}
+            //Console.WriteLine($"{counter} ta juft sonlar mavjud");
+
+            //Console.Write("List miqdorini kiriting : ");
+            //var n = int.Parse(Console.ReadLine());
+            //List<int> ints = new List<int>();
+            //var counter = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("List elementlarini kiriting : ");
+            //    var nums = int.Parse(Console.ReadLine());
+            //    ints.Add(nums);
+            //    if (nums % 2 != 0)
+            //    {
+            //        counter = counter + 1;
+            //    }
+            //}
+            //Console.WriteLine($"{counter} ta toq sonlar mavjud");
+
+            //Console.Write("List miqdorini kiriting : ");
+            //var n = int.Parse(Console.ReadLine());
+            //List<int> ints = new List<int>();
+            //var counter = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("List elementlarini kiriting : ");
+            //    var nums = int.Parse(Console.ReadLine());
+            //    ints.Add(nums);
+            //    if (nums % 3 == 0 && nums % 7 == 0)
+            //    {
+            //        counter = counter + 1;
+            //    }
+            //}
+            //Console.WriteLine($"{counter} ta 3,7 bo'linadigan sonlar bor");
+
+            //Console.Write("List miqdorini kiriting : ");
+            //var n = int.Parse(Console.ReadLine());
+            //List<int> ints = new List<int>();
+            //var counter = 0;
+            //var total = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("List elementlarini kiriting : ");
+            //    var nums = int.Parse(Console.ReadLine());
+            //    ints.Add(nums);
+            //    if (nums >= 10 && nums % 2 != 0)
+            //    {
+            //        counter += 1;
+            //        total += nums;
+            //    }
+            //}
+            //Console.WriteLine($"{counter} : miqdori");
+            //Console.WriteLine($"{total} : yigindisi");
+ 
+        }
+    }
+}
