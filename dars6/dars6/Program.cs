102874de119fc519b88d7f167409c8b20aca697a
Bekzod <bekzodjonxasanov7@gmail.com>
2025-12-13 14:28:35 +0500

    6-dars qo'shildi

---
+++ dars6/dars6/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace dars6;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine(Add("salom", "salom"));
+        //Console.WriteLine(Add(1.1f, 1.1f));
+        //Console.WriteLine(Add(1.1f, 1.1f, 1.1f));
+        //Console.WriteLine(Calculate(2, 2, 2));
+        //Console.WriteLine(Calculate(2, 2));
+        //Console.WriteLine(Multiplicate(5));
+        //Console.WriteLine(Multiplicate(4,7));
+    }
+    //static float Add(float a, float b)
+    //{
+    //    return a + b;
+    //}
+    //static float Add(float a, float b, float c)
+    //{
+    //    return a + b + c;
+    //}
+    //static string Add(string a, string b)
+    //{
+    //    return a + b;
+    //}
+
+    //static int Calculate(int a, int b)
+    //{
+    //    return a + b;
+    //}
+    //static int Calculate(int a, int b, int c)
+    //{
+    //    return a * b * c;
+    //}
+
+    //static int Multiplicate(int a)
+    //{
+    //    var yigindi = 1;
+    //    for (int i = 1; i <= a; i++)
+    //    {
+    //        yigindi = yigindi * i;
+    //    }
+    //    return yigindi;
+    //}
+    //static int Multiplicate(int a, int b)
+    //{
+    //    var yigindi = 1;
+    //    for (int i = a; i <= b; i++)
+    //    {
+    //        yigindi = yigindi * i;
+    //    }
+    //    return yigindi;
+    //}
+    //static int Multiplicate(int a, int b, int c)
+    //{
+    //    return (a * b) * c;
+    //}
+}
